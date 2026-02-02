e549f5adbff3bc9365e1d8cc64dd0d076518f375
leonwunderlichderfield-lgtm <leonwunderlichderfield@gmail.com>
2026-02-02 11:58:37 +0000

    finish task 1 on lab week 2

--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 
 int main() {
     int sensorID = 99;
+    float tempRead = 12.34;
+    char status = 'A';
 
+    printf("The sensor ID: %d, \nthe temperature reading is: %.2f, \nstatus code: %c", sensorID, tempRead, status);
     return 0;
 }
